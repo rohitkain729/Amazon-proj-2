940dbe5b897079c066ac47a6c51f575d9e5a63e8
rohit verma <rohit729verma@gmail.com>
2026-02-16 13:26:19 +0530

    save cart in local Storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -6,12 +9,17 @@ export let cart = [
   {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1,
-  },
-  {
-    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    quantity: 1,
-  },
+  }
+  
 ];
+
+}
+
+
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -29,7 +37,7 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
-  console.log(cart);
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -43,5 +51,6 @@ export function removeFromCart(productId) {
     }
   });
   cart = newCart;
+  saveToStorage();
   // add each product to the new array,except for this productId
 }
