eae79de36487729d30cbf985f90071099fb90a6a
rohit verma <rohit729verma@gmail.com>
2026-02-28 11:01:00 +0530

    Create class version of cart

---
+++ data/cart-class.js
@@ -0,0 +1,76 @@
+class Cart {
+  cartItems;
+  localStorageKey;
+
+  constructor(localStorageKey) {
+    this.localStorageKey = localStorageKey;
+    this.loadFromStorage();
+  }
+
+  loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+    if (!this.cartItems) {
+      this.cartItems = [];
+    }
+  }
+
+  saveToStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+
+  addToCart(productId) {
+    let matchingItem;
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      this.cartItems.push({
+        productId: productId,
+        quantity: 1,
+        deliveryOptionId: "1",
+      });
+    }
+    this.saveToStorage();
+  }
+
+  removeFromCart(productId) {
+    // create a new arry
+    const newCart = [];
+
+    // loop through the cart
+    this.cartItems.forEach((cartItem) => {
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    });
+    this.cartItems = newCart;
+    this.saveToStorage();
+    // add each product to the new array,except for this productId
+  }
+
+  updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+    matchingItem.deliveryOptionId = deliveryOptionId;
+    this.saveToStorage();
+  }
+}
+
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
+
+console.log(cart);
+console.log(businessCart);
+
+console.log(businessCart instanceof Cart);
+
+cart.loadFromStorage();
