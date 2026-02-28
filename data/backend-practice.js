c5a6652349e47d5e3ec8499d9746064668768237
rohit verma <rohit729verma@gmail.com>
2026-02-28 21:18:24 +0530

    Add backend Practise file

---
+++ data/backend-practice.js
@@ -0,0 +1,13 @@
+const xhr = new XMLHttpRequest();
+
+let data;
+xhr.addEventListener("load", () => {
+  data = xhr.response;
+  console.log(data);
+});
+
+xhr.open("GET", "https://supersimplebackend.dev/");
+xhr.send();
+
+
+
