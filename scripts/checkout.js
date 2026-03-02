1d80d97de95b1388c1ff78753606ca5ed499e155
rohit verma <rohit729verma@gmail.com>
2026-03-02 15:30:50 +0530

    use asyn and await ,made a function use load product and cart with it

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,23 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
+async function loadPage() {
+  await loadProductsFetch();
+
+  const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve("value33");
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+
+  return "value2";
+}
+loadPage();
+
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -17,6 +34,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 /*
 new Promise((resolve) => {
