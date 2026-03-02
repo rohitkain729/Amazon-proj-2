78f5ff24ccf16f9578367b7875f1a081d71668ba
rohit verma <rohit729verma@gmail.com>
2026-03-02 15:01:29 +0530

    use the fetch command to load product ,also use promise all

--- data/products.js
+++ data/products.js
@@ -583,6 +583,32 @@ export const products = [
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch("https://supersimplebackend.dev/products/")
+    .then((response) => {
+      // console.log(response.json());
+      return response.json();
+    })
+    .then((productsData) => {
+      products = productsData.map((productDetails) => {
+        if (productDetails.type === "clothing") {
+          return new Clothing(productDetails);
+        }
+
+        return new Product(productDetails);
+      });
+      console.log("load products");
+    });
+  return promise;
+}
+
+/*
+loadProductsFetch().then((resolve) => {
+  console.log("next step");
+});
+
+*/
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -594,12 +620,10 @@ export function loadProducts(fun) {
 
       return new Product(productDetails);
     });
-    console.log('load products');
+    console.log("load products");
     fun();
   });
   const url = "https://supersimplebackend.dev/products/";
   xhr.open("GET", url);
   xhr.send();
 }
-
-
